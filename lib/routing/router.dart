a5709d4b9e5d03718c71fee2c72b8ed9612f2687
Marin <10130438+mrn@users.noreply.github.com>
2022-11-25 19:32:18 +0100

    aaaaaa

--- lib/routing/router.dart
+++ lib/routing/router.dart
@@ -1,3 +1,28 @@
+import 'package:ehh/screens/auth_screen/auth_screen.dart';
+import 'package:ehh/screens/home/home_screen.dart';
+import 'package:ehh/screens/settings/settings_dart.dart';
 import 'package:go_router/go_router.dart';
 
-final router = GoRouter(routes: []);
+final router = GoRouter(
+  routes: [
+    ShellRoute(
+      builder: (context, state, child) {
+        return AuthScreen();
+      },
+      routes: [
+        GoRoute(
+          path: '/',
+          builder: (context, state) {
+            return HomeScreen();
+          },
+        ),
+        GoRoute(
+          path: '/settings',
+          builder: (context, state) {
+            return SettingsScreen();
+          },
+        ),
+      ],
+    ),
+  ],
+);
---
+++ lib/screens/auth_screen/auth_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
