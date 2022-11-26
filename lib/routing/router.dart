2d768df3a590801ed69b7cdda04188893d6501bb
Idkwhoami42 <neellodha42@gmail.com>
2022-11-26 08:36:31 +0100

    phone 2

--- lib/routing/router.dart
+++ lib/routing/router.dart
@@ -1,4 +1,5 @@
 import 'package:ehh/screens/auth_screen/auth_screen.dart';
+import 'package:ehh/screens/auth_screen/otp_screen.dart';
 import 'package:ehh/screens/home/home_screen.dart';
 import 'package:ehh/screens/settings/settings_dart.dart';
 import 'package:go_router/go_router.dart';
@@ -11,7 +12,13 @@ final router = GoRouter(
       },
       routes: [
         GoRoute(
-          path: '/',
+          path: '/otp',
+          builder: (context, state) {
+            return OtpScreen();
+          },
+        ),
+        GoRoute(
+          path: '/home',
           builder: (context, state) {
             return HomeScreen();
           },
--- lib/screens/auth_screen/auth_screen.dart
+++ lib/screens/auth_screen/auth_screen.dart
@@ -1,5 +1,6 @@
 import 'package:ehh/constants/spacing.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({
@@ -16,41 +17,46 @@ class _AuthScreenState extends State<AuthScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                  horizontal: Spacing.screenPadding,
-                  vertical: Spacing.screenPadding,
-                ),
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: "Phone number",
-                    enabledBorder: UnderlineInputBorder(),
-                    focusedBorder: UnderlineInputBorder(),
+    return Navigator(
+      onGenerateRoute: (_) => MaterialPageRoute(
+        builder: (ctx) => Scaffold(
+          body: SafeArea(
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: Spacing.screenPadding,
+                    ),
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        hintText: "Phone number",
+                        enabledBorder: UnderlineInputBorder(),
+                        focusedBorder: UnderlineInputBorder(),
+                      ),
+                      validator: (value) {
+                        final czechFormat = RegExp(r"(2[0-9]{2}|3[0-9]{2}|4[0-9]{2}|5[0-9]{2}|72[0-9]|73[0-9]|77[0-9]|60[1-8]|56[0-9]|70[2-5]|79[0-9])[0-9]{3}[0-9]{3}$");
+                        if (value == null) return "Enter a phone number";
+                        if (!czechFormat.hasMatch(value)) return "Invalid phone number";
+                      },
+                      onChanged: (value) => _phone = value,
+                      keyboardType: TextInputType.phone,
+                    ),
                   ),
-                  validator: (value) {
-                    final czechFormat = RegExp(r"(2[0-9]{2}|3[0-9]{2}|4[0-9]{2}|5[0-9]{2}|72[0-9]|73[0-9]|77[0-9]|60[1-8]|56[0-9]|70[2-5]|79[0-9])[0-9]{3}[0-9]{3}$");
-                    if (value == null) return "Enter a phone number";
-                    if (!czechFormat.hasMatch(value)) return "Invalid phone number";
-                  },
-                  onChanged: (value) => _phone = value,
-                  keyboardType: TextInputType.phone,
-                ),
+                  TextButton(
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        debugPrint(_phone);
+                        context.go("/otp");
+                      }
+                    },
+                    child: Text("Send OTP"),
+                  )
+                ],
               ),
-              TextButton(
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    debugPrint(_phone);
-                  }
-                },
-                child: Text("Sign up"),
-              )
-            ],
+            ),
           ),
         ),
       ),
