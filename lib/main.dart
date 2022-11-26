20c8612b83763cd81fc93e0d4810ec3f17ac7043
Marin <10130438+mrn@users.noreply.github.com>
2022-11-26 13:10:48 +0100

    add flutterfire shit

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:ehh/app.dart';
 import 'package:ehh/controllers/permissions_controller.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
- runApp(
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  runApp(
     MultiProvider(
       providers: [
         ChangeNotifierProvider(create: (_) => PermissionsController()),
