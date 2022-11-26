07c52f7e576a73273feb1ccb98de98038754a13f
Idkwhoami42 <neellodha42@gmail.com>
2022-11-26 21:01:06 +0100

    map

--- lib/controllers/permissions_controller.dart
+++ lib/controllers/permissions_controller.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:location/location.dart' as location;
 import 'package:permission_handler/permission_handler.dart';
 
 class PermissionsController extends ChangeNotifier {
@@ -6,8 +7,49 @@ class PermissionsController extends ChangeNotifier {
   Map<Permission, PermissionStatus>? get permissions => _permissions;
   Map<Permission, PermissionStatus>? _permissions;
 
+  /// Service permission statuses.
+  Map<Permission, ServiceStatus>? get servicePermissions => _servicePermissions;
+  Map<Permission, ServiceStatus>? _servicePermissions;
+
   PermissionsController() {
     _init();
+  }
+
+  /// Request permissions.
+  /// Return true if all requested permissions were granted.
+  Future<bool> requestPermission(List<Permission> requestedPermissions) async {
+    if (_permissions == null) {
+      return false;
+    }
+    var result = true;
+    Map<Permission, PermissionStatus> requestResult =
+        await requestedPermissions.request();
+    for (var entry in requestResult.entries) {
+      result &= entry.value == PermissionStatus.granted;
+      _permissions?[entry.key] = entry.value;
+    }
+    notifyListeners();
+    return result;
+  }
+
+  /// Request services.
+  /// Return true of all requested service permissions were given.
+  Future<bool> requestService(
+      List<Permission> requestedServicePermissions) async {
+    if (_servicePermissions == null) return false;
+    var result = true;
+    for (Permission permission in requestedServicePermissions) {
+      if (permission == Permission.location) {
+        var isGranted = await location.Location().requestService();
+        result &= isGranted;
+        if (isGranted) {
+          _servicePermissions![Permission.location] = ServiceStatus.enabled;
+        }
+      }
+    }
+    notifyListeners();
+    return result;
+
   }
 
   Future<void> _init() async {
@@ -19,7 +61,6 @@ class PermissionsController extends ChangeNotifier {
     perms[Permission.camera] = await Permission.camera.status;
     perms[Permission.microphone] = await Permission.microphone.status;
     perms[Permission.notification] = await Permission.notification.status;
-
     // update controller data
     _permissions = perms;
     notifyListeners();
