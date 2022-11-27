031c2c0aee5e0b62f30d8da3931692a5b8bf3322
Marin <10130438+mrn@users.noreply.github.com>
2022-11-27 01:42:29 +0100

    asdaljkwd

--- lib/services/firestore/firestore_references.dart
+++ lib/services/firestore/firestore_references.dart
@@ -3,11 +3,16 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class ColRefs {
   // Collections
   static final colUsers = FirebaseFirestore.instance.collection("users");
+  static final colEmergencies =
+      FirebaseFirestore.instance.collection("emergencies");
 }
 
 class DocRefs {
-  // Documents
   static DocumentReference<Map<String, dynamic>> user(String userId) {
     return ColRefs.colUsers.doc(userId);
+  }
+
+  static DocumentReference<Map<String, dynamic>> emergency(String emergencyId) {
+    return ColRefs.colEmergencies.doc(emergencyId);
   }
 }
